cee1efba388493efe15f341f353b3d9fe1c955e4
Alejandra Patino <ale@alejandras-mbp.home>
2020-08-04 11:07:15 -0700

    added routes and controller actions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const port = 3000; 
+const indexRouter = require('./routes/index');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/', indexRouter);
 
 app.listen(port, function() {
     console.log(`Express is listening on port:${port}`);
