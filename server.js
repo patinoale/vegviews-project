1cc6498b74ce77fadb867bd306efc9635b3a95e0
Alejandra Patino <ale@alejandras-mbp.home>
2020-08-04 11:00:31 -0700

    created directories and base files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const port = 3000; 
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(port, function() {
+    console.log(`Express is listening on port:${port}`);
+});
